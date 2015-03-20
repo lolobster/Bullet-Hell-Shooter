261a0387b22c013b8c54b43e8fc58b536128891e
lolobster <nissinena@elisanet.fi>
2015-03-20 12:12:56 +0200

    lisää

--- bullet hell gaem plox/bullet hell gaem plox/Player.cpp
+++ bullet hell gaem plox/bullet hell gaem plox/Player.cpp
@@ -45,22 +45,22 @@ void Player::playerController(float deltaTime)
 			if (sf::Keyboard::isKeyPressed(kb_left))
 			{
 				// liikettä vasempaan
-				posX += -0.2 * deltaTime;
+				posX -= +0.5 * deltaTime;
 			}
 			if (sf::Keyboard::isKeyPressed(kb_right))
 			{
 				// liikettä oikeaan
-				posX += 0.2 * deltaTime;
+				posX += 0.5 * deltaTime;
 			}
 			if (sf::Keyboard::isKeyPressed(kb_forward))
 			{
 				// liikettä ylöspäin
-				posY += 0.2 * deltaTime;
+				posY -= 0.5 * deltaTime;
 			}
 			if (sf::Keyboard::isKeyPressed(kb_reverse))
 			{
 				// liikettä alaspäin
-				posY += -0.2 * deltaTime;
+				posY += 0.5 * deltaTime;
 			}
 
 			pl_sprite.setPosition(posX, posY);
